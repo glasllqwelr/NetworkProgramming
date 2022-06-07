cc1c975d826a0b2433ef964f0750bfc71101fa59
glasllqwelr <100193311+glasllqwelr@users.noreply.github.com>
2022-06-07 14:19:26 +0900

    Add files via upload

---
+++ Graphiccard.java
@@ -0,0 +1,13 @@
+package Danawa;
+public class Graphiccard extends computer{
+
+@Override
+public void order() {
+System.out.println("gtx1080ti을장바구니에넣었다.");
+
+}
+public void money() {
+System.out.println("가격이60만원이다.");
+
+}
+}
